7a945dcd500665a328aa276363b24530a2f875d9
Vinícius Albuquerque <vinicius.albuquerque@outlook.com>
2019-06-26 15:00:04 -0300

    added store method for UserControler

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+// Controller
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Teste',
-    email: 'teste@teste.com.br',
-    password_hash: '234234',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
