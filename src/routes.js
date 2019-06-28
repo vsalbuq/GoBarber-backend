1b78f7c0034835de8435781ab4a29e7fac11e97b
Vinícius Albuquerque <vinicius.albuquerque@outlook.com>
2019-06-28 11:58:21 -0300

    adds multer dependency and sets route to upload files

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,9 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+// Config
+import multerConfig from './config/multer';
+
 // Controllers
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
@@ -7,11 +12,17 @@ import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
+const upload = multer(multerConfig);
+
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json(req.file);
+});
 
 export default routes;
