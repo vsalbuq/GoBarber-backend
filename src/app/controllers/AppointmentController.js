13b2c51fe1311d491a9f339643c6aa34dc74fa21
Vinícius Albuquerque <vinicius.albuquerque@outlook.com>
2019-07-02 18:07:07 -0300

    feat: when an appointment is cancelled, an email is sent to the provider

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -8,6 +8,9 @@ import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
 
+// Features
+import Mail from '../../lib/Mail';
+
 class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query;
@@ -120,7 +123,15 @@ class AppointmentController {
 
   async delete(req, res) {
     console.log(req.params.id);
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     if (!appointment) {
       return res
@@ -146,6 +157,12 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Appointment cancelled',
+      text: 'One of your appointments was cancelled.',
+    });
 
     return res.json(appointment);
   }
